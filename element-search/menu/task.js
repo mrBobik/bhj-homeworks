a4ba90f42ecc45a5d0abf6b29f3ac86626cf0c7a
mrBobik <113882549+mrBobik@users.noreply.github.com>
2023-11-30 22:36:39 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,9 +1,10 @@
 const menuLink = Array.from(document.getElementsByClassName('menu__link'));
-
+console.log(menuLink)
 
 menuLink.forEach(el => {
     el.onclick = function () {
         let parent = el.parentElement.querySelector('ul');
+        console.log(parent);
         if (parent.classList.contains('menu_active')) {
             parent.classList.remove('menu_active');
         } else {parent.classList.add('menu_active')}
