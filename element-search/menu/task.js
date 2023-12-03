31efc59182c1f7d8b7ddefcaf73a31b26a7ec28b
mrBobik <113882549+mrBobik@users.noreply.github.com>
2023-12-03 14:07:42 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,12 @@
 const menuLink = Array.from(document.getElementsByClassName('menu__link'));
-console.log(menuLink)
+
 
 menuLink.forEach(el => {
     el.onclick = function () {
         let parent = el.parentElement.querySelector('ul');
-        console.log(parent);
-        if (parent.classList.contains('menu_active')) {
+        if (parent !==null && parent.classList.contains('menu_active')) {
             parent.classList.remove('menu_active');
-        } else {parent.classList.add('menu_active')}
+        } else {parent !==null && parent.classList.add('menu_active')}
         return false;
     };
 })
