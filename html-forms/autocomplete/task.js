4231cd82b47cf0ecdb72fac33a36e16484d2eca2
mrBobik <113882549+mrBobik@users.noreply.github.com>
2023-12-03 17:25:48 +0300

    Update task.js

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -1,93 +1,41 @@
-class Autocomplete {
-  constructor( container ) {
-    this.container = container;
-    this.input = container.querySelector( '.autocomplete__input' );
-    this.searchInput = container.querySelector( '.autocomplete__search' );
-    this.list = container.querySelector( '.autocomplete__list' );
-    this.valueContainer = container.querySelector( '.autocomplete__value' );
-    this.valueElement = container.querySelector( '.autocomplete__text-content' );
+let interestsMain = document.querySelector('.interests_main'),
+    interestCheck = interestsMain.querySelectorAll('.interest'),
+    checkedInterests,
+    allInterests,
+    parentInterest,
+    main;
 
-    this.registerEvents();
-  }
+interestCheck.forEach(el => {
+    el.addEventListener('change', function(event) {
+        main = event.target.closest('.interest').querySelector('.interests_active'); 
+        
+        if (main !== null) {
+            main.querySelectorAll('.interest__check').forEach(e => {
+                e.checked = event.target.checked;
+            });
+        }
+        
 
-  registerEvents() {
-    this.valueContainer.addEventListener( 'click', e => {
-      this.searchInput.classList.add( 'autocomplete__search_active' );
-      this.list.classList.add( 'autocomplete__list_active' );
-      this.searchInput.value = this.valueElement.textContent.trim();
-      this.searchInput.focus();
+        if(el.closest('.interests').classList.contains('interests_active')) {
+            parentInterest = el.closest('.interests').closest('.interest').querySelector('.interest__check');
+            console.log('parentInterest:'+ parentInterest);
+            allInterests = el.closest('.interests').querySelectorAll('.interest__check');
+            checkedInterests = el.closest('.interests').querySelectorAll('.interest__check:checked');
+        } else {
+            parentInterest = el.querySelector('.interest__check');
+            allInterests = el.querySelectorAll('.interest__check');
+            checkedInterests = el.querySelectorAll('.interest__check:checked');
+        }
 
-      this.onSearch();
+        if (checkedInterests.length === 0) {
+            parentInterest.indeterminate = false;
+            parentInterest.checked = false;
+        } else if (checkedInterests.length < allInterests.length) {
+            parentInterest.indeterminate = true;
+            parentInterest.checked = false;
+        } else if (checkedInterests.length === allInterests.length) {
+            parentInterest.indeterminate = false;
+            parentInterest.checked = true;
+        }
     });
-
-
-    this.searchInput.addEventListener( 'input', e => this.onSearch());
-
-    this.list.addEventListener( 'click', e => {
-      const { target } = e;
-      if ( !target.matches( '.autocomplete__item' )) {
-        return;
-      }
-
-      const { textContent: text } = target,
-        { id: value, index } = target.dataset;
-
-      this.onSelect({
-        index,
-        text,
-        value
-      });
-    });
-  }
-
-  onSelect( item ) {
-    this.input.selectedIndex = item.index;
-    this.valueElement.textContent = item.text;
-
-    this.searchInput.classList.remove( 'autocomplete__search_active' );
-    this.list.classList.remove( 'autocomplete__list_active' );
-  }
-
-  onSearch() {
-    const matches = this.getMatches( this.searchInput.value );
-
-    this.renderMatches( matches );
-  }
-
-  renderMatches( matches ) {
-    const html = matches.map( item => `
-    	<li>
-        <span class="autocomplete__item"
-        	data-index="${item.index}"
-          data-id="${item.value}"
-        >${item.text}</span>
-      </li>
-    `);
-
-    this.list.innerHTML = html.join('');
-  }
-
-  getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
-  }
-}
-
-new Autocomplete( document.querySelector( '.autocomplete' ));
+});
