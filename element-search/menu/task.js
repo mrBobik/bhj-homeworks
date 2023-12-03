012be8dc3d02585e18b8a756717899ff1f014ecd
mrBobik <113882549+mrBobik@users.noreply.github.com>
2023-12-03 16:15:05 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,12 +1,19 @@
-const menuLink = Array.from(document.getElementsByClassName('menu__link'));
+let menuLink = document.querySelectorAll("ul.menu.menu_main > li.menu__item > a.menu__link");
 
 
-menuLink.forEach(el => {
-    el.onclick = function () {
-        let parent = el.parentElement.querySelector('ul');
-        if (parent !== null && parent.classList.contains('menu_active')) {
-            parent.classList.remove('menu_active');
-        } else { parent !== null && parent.classList.add('menu_active') }
-        return false;
-    };
-})
+menuLink.forEach(e => {
+    e.onclick = () => {
+        let menuActive = document.querySelectorAll("ul.menu.menu_main > li.menu__item > ul.menu.menu_sub.menu_active");
+        console.log(menuActive);
+        if (e.closest(".menu__item").querySelector("ul.menu.menu_sub") !== null) {            
+            if (menuActive.length !== 0 && menuActive[0].closest(".menu__item") !== e.closest(".menu__item")) {
+                menuActive[0].classList.remove("menu_active");
+            }
+            e.closest(".menu__item").querySelector("ul.menu.menu_sub").classList.toggle("menu_active");
+            let sd = e.closest(".menu__item").querySelector("ul.menu.menu_sub");
+            console.log(sd);
+
+            return false;
+        }
+    }
+});
